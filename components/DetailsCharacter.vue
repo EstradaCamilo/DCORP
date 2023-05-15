ec4003241256c8ecff6a2ecb9dba8fc2535021b3
EstradaCamilo <estradasalinasjosecamilo@gmail.com>
2023-05-15 10:38:06 -0500

    Name and description

--- components/DetailsCharacter.vue
+++ components/DetailsCharacter.vue
@@ -3,7 +3,7 @@
         <Dialog as="div" @close="charactersStore.setIsOpenDetailsCharacter(false)" class="relative z-10">
             <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0" enter-to="opacity-100"
                 leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0">
-                <div class="fixed inset-0 bg-black bg-opacity-25" />
+                <div class="fixed inset-0 bg-black bg-opacity-50" />
             </TransitionChild>
 
             <div class="fixed inset-0 overflow-y-auto">
@@ -11,17 +11,25 @@
                     <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0 scale-95"
                         enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100"
                         leave-to="opacity-0 scale-95">
-                        <DialogPanel
-                            class="details-character-marvel">
-                            xd
+                        <DialogPanel class="details-character-marvel">
                             <div class="details-character-marvel--bg"></div>
                             <div class="details-character-marvel--content">
-                                <button type="button"
+                                <div class="inline-flex justify-between items-center">
+                                    <img class="h-7 w-7" src="~/assets/images/border-l.svg" alt="border left">
+                                    <span class="px-2 text-sm text-center text-primary-100">
+                                        {{ charactersStore.currentCharacters.name ?? 'Nombre' }}
+                                    </span>
+                                    <img class="h-7 w-7" src="~/assets/images/border-r.svg" alt="border right">
+                                </div>
+                                <p class="text-xs font-light text-primary-100">
+                                    {{ charactersStore.currentCharacters.description ?? 'Descripción' }}
+                                </p>
+                            </div>
+                            <!-- <button type="button"
                                 class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                                 @click="charactersStore.setIsOpenDetailsCharacter(false)">
                                 Close
-                            </button>
-                        </div>
+                            </button> -->
                         </DialogPanel>
                     </TransitionChild>
                 </div>
@@ -43,9 +51,10 @@ const charactersStore = useCharactersStore()
 </script>
 
 <style lang="scss" scope>
-.details-character-marvel{
+.details-character-marvel {
     @apply w-full max-w-md transform overflow-hidden rounded-md p-6 text-left align-middle shadow-xl transition-all;
     @apply relative border border-grey-500 border-opacity-40;
+
     &--bg {
         @apply absolute top-0 left-0 z-10 h-full w-full rounded-md bg-cover;
         background-image: url('~/assets/images/bg-card-character.png');
@@ -54,13 +63,8 @@ const charactersStore = useCharactersStore()
     }
 
     &--content {
-        @apply flex flex-col justify-between z-20;
+        @apply relative flex flex-col gap-2 justify-between z-30;
     }
-
-    &--details {
-        @apply grid gap-4;
-    }
-
 
     &::after {
         @apply absolute top-0 left-0 h-full w-full mix-blend-multiply content-[''] rounded-md;
@@ -71,5 +75,4 @@ const charactersStore = useCharactersStore()
         @apply absolute top-0 left-0 h-full w-full mix-blend-multiply content-[''] rounded-md;
         background: linear-gradient(360deg, #000000 0%, #090C14 0.01%, rgba(23, 26, 32, 0) 36.49%, rgba(39, 44, 52, 0) 114.84%);
     }
-}
-</style>
+}</style>
